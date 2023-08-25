70adadbe0b28836056426460a5be8a7a276f103d
Micha Buchholz <mail@lean-stack.de>
2023-08-25 09:50:22 +0200

    Create initial static template.

---
+++ template/component.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Component View</title>
+</head>
+<body>
+<h2>Component View</h2>
+</body>
+</html>
---
+++ template/index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package     Weltspiegel
+ *
+ * @copyright   Copyright (C) Weltspiegel Cottbus. All rights reserved.
+ * @license     GNU General Public License version 3 or later; see LICENSE
+ *
+ * @since 0.0.1
+ */
+
+\defined('_JEXEC') or die;
+?>
+
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Weltspiegel Cottbus</title>
+</head>
+<body>
+<h1>Weltspiegel Cottbus</h1>
+</body>
+</html>
